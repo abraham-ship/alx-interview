7a5ff3d92022593422f895e92f0ce43ebb44f046
abraham-ship <abuzsein3@gmail.com>
2023-12-03 04:06:37 +0000

    initial commit

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from math import factorial
+
+
+def pascal_triangle(n):
+    '''print pascal's triangle'''
+    triangle = []
+
+    for i in range(n):
+        # loop to get leading spaces
+        row = [1] * [i + 1]
+
+        # loop to get elements of row i
+        for j in range(1, i):
+            row[j] = triangle[j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
