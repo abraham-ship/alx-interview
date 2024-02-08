e1f536193f7f720e639443def5a7e6775a863843
abraham-ship <abuzsein3@gmail.com>
2024-02-08 19:03:24 +0000

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    '''calculate perimeter of an island and
+    return the perimeter of the island described in grid'''
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Assume all sides are land
+
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract 2 for shared edge
+
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract 2 for shared edge
+
+    return perimeter
